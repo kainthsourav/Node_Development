f4b396584e2a82836442291f3df36ed813f7618b
kainthsourav <souravkainth@outlook.com>
2020-08-19 00:07:25 +0530

    Importing npm Modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,10 +4,16 @@
 
 // fs.appendFileSync('notes.txt',' Appened text ')
 
-const add=require('./utiles');
-const notes=require('./notes');
+// const add=require('./utiles');
+// const notes=require('./notes');
 
-const sum=add(4,4);
-console.log(sum);
-const n=notes();
-console.log(n);
+// const sum=add(4,4);
+// console.log(sum);
+// const n=notes();
+// console.log(n);
+
+const validator=require('validator');
+
+console.log(validator.isEmail('souravkainth@outlo'));
+
+console.log(validator.isURL('https://zinny.com'));

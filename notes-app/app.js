7dbd605b9f742d2f1cab7102906d012a18b56f69
kainthsourav <souravkainth@outlook.com>
2020-08-20 08:42:26 +0530

    Parsing with Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,59 @@
 const chalk=require('chalk');
-const validator=require('validator');
+const validator=require('validator')
+const yargs=require('yargs');
+const { command, string, describe } = require('yargs');
+
+//custom verison
+yargs.version('1.1.0');
 
 
-cmd=process.argv[2];
+//Creating commands
+//add
+yargs.command({
+    command:'add',
+    describe:'Adds a new note',
+    builder:{
+        title:{
+            describe:'Note Title',
+           demandoption:true,
+            type:string
+        },
+        body:{
+            describe:'Note description',
+            demandoption:true,
+            type:string
+        }
+    },
+    handler:function(argv)
+    {
+        console.log('Title: '+argv.title)
+        console.log('Description: '+argv.description)
+    }
+})
+//remove
+yargs.command({
+    command:'remove',
+    describe:'Remove a note',
+    handler:function()
+    {
+        console.log('Note removed')
+    }
+})
+//list
+yargs.command({
+    command:'list',
+    describe:'Shows list of notes',
+    handler:function(){
+        console.log('Here is list of all notes');
+    }
+})
+//read
+yargs.command({
+    command:'read',
+    describe:'read the selected note',
+    handler:function(){
+        console.log('opening selected note')
+    }
+})
 
-if(cmd==='add')
-{
- console.log(chalk.white.bgGreen('Note has been added'));
-}
-else if(cmd==='remove')
-{
-    console.log(chalk.white.bgRed('Note has been removed'));
-}
-else{
-    console.log('Invalid',process.argv[2]);
-}
+yargs.parse()

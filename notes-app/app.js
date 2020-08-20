d6854f2591f243aacad840b93d99af56103a6f80
kainthsourav <souravkainth@outlook.com>
2020-08-20 07:22:10 +0530

    Using Chalk and nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,7 @@
-// const fs=require('fs');
-
-//fs.writeFileSync('notes.txt','This is file created by node');
-
-// fs.appendFileSync('notes.txt',' Appened text ')
-
-// const add=require('./utiles');
-// const notes=require('./notes');
-
-// const sum=add(4,4);
-// console.log(sum);
-// const n=notes();
-// console.log(n);
-
+const chalk=require('chalk');
 const validator=require('validator');
 
-console.log(validator.isEmail('souravkainth@outlo'));
 
-console.log(validator.isURL('https://zinny.com'));
+console.log(chalk.green.bold('Sourav'));
+console.log(chalk.inverse.red.bold('Sourav'));
+console.log(chalk.white.bgRed('Kainth'));
